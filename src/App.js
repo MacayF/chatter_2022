c46d052599f17d19b7fa9f1e5c1d16346658f9d7
Macay Fischer <macayfischer@Macays-MacBook-Pro.local>
2022-01-28 15:40:26 -0800

    styling 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+// html written in JS
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <img className="logo"/>
-        <span className="title">Chatter!</span>
+        <div className="upper-header">
+          <span className="subtitle">Macay Fischer Chat App</span>
+          <img className="logo"/>
+        </div>
+        <div className="lower-header">
+          <span className="title">Talking Space.™</span>
+        </div>
       </header>
       <footer className="footer">
         <input className="text-input"/>
-        <button className="send">↑</button>
+        <button className="send">send ↑</button>
       </footer>
     </div>
   );
